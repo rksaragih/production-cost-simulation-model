2f00f66178f9129b0dd304deb87c6ab7df494d9c
rksaragih <168797432+rksaragih@users.noreply.github.com>
2026-05-10 15:07:04 +0700

    Import CORS

--- src/app.py
+++ src/app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  
 
 # =========================
 # LOAD MODEL
